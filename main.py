eb33ac72fd269f80d183f46f37bbc1b2b458a8e9
VladFearo <69718459+VladFearo@users.noreply.github.com>
2021-11-13 17:22:25 +0200

    yee

--- main.py
+++ main.py
@@ -39,5 +39,78 @@ def onlyPositive(f):
     return func
 
 
-g = onlyPositive(f1)
-print(g(2))
+def interceptPoint(t1, t2):
+    """
+    Finds an interception point between two functions that look like y=mx+n
+
+    :param t1: A tuple containing two points representing the m and n of the 1st function that you want to find an
+    interception point between
+    :param t2: A tuple containing two points representing the m and n of the 2nd function that you want to find an
+    interception point between
+    :return: A tuple containing the point of interception between two functions
+    """
+    m1, m2, n1, n2 = t1[0], t2[0], t1[1], t2[1]
+    mx = m2 - m1  # m2x+n2 = m1x+n1 -> m2x-m1x = n1 - n2
+    n = n1 - n2
+    if mx != 0:
+        x = n/mx
+    else:
+        return None
+    y = m1*x+n1  # substituting the x into one of the functions in order to find y
+    answer = (x, y)
+    return answer
+
+
+def printNumbers(start, end, n):
+    """
+    Prints all numbers from a starting number till an end number in jumps of 1 while skipping a number specified
+
+    :param start: A number that you start from
+    :param end: A number on which you end
+    :param n: A number you skip
+    :return: None
+    """
+    if start != n:
+        print(start)
+    if start > end:
+        if start == end - 1:
+            return
+        printNumbers(start - 1, end, n)
+    if start < end:
+        if start == end + 1:
+            return
+        printNumbers(start+1, end, n)
+
+
+
+
+def arrProduct(arr1, arr2):
+    """
+    Creates an array containing numbers from one array times the numbers from a second array
+    (the arrays must be of the same size)
+    (e.g. arr1 = [1,2,3], arr2 = [1,2,3], newArr = [1,2,2,3,3,3])
+
+    :param arr1: An array of non negative integers
+    :param arr2: An array of non negative integers
+    :return: An array containing numbers from one array times the numbers from a second array
+    """
+    newArr = []
+    for i in range(len(arr1)):
+        for j in range(arr2[i]):
+            newArr.append(arr1[i])
+    return newArr
+
+def analyze(string):
+    """
+    Counting the number of rainy months that are defined as months with more then 75 mm of rain
+
+    :param string: A string containing numbers
+    :return: A number of months that are rainy
+    """
+    rainy_months = 0
+    arr = string.split(", ")
+    for x in arr:
+        if float(x) > 75:
+            rainy_months += 1
+    return rainy_months
+
